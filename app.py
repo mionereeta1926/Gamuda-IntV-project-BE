8d28fcf891c63d80516fecce91a48ec24e8bb8a9
mionereeta1926 <mionereeta1926@gmail.com>
2026-05-19 14:47:20 +0800

    19/5/2025 - azure deployment items

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
-import services.logging_service
-
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
-from routes.upload import router as upload_router
-from routes.chat import router as chat_router
-
-app = FastAPI(title="Project Intelligence Assistant")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(upload_router)
-app.include_router(chat_router)
+app = FastAPI()
 
 @app.get("/")
-def health_check():
-    return {
-        "status": "running"
-    }
+def home():
+    return {"message": "API running"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
